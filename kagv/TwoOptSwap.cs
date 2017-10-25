69056cc82983628d171332195e862690e7fa5848
YannisMenex <johnmenex@gmail.com>
2017-10-25 15:54:39 +0300

    Some cleaning up

--- kagv/TwoOptSwap.cs
+++ kagv/TwoOptSwap.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace kagv {
+﻿namespace kagv {
     public static class TwoOptSwap {
         public static int[] OptSwap(int[] TempTour, int i, int v) {
             int size = TempTour.Length;
